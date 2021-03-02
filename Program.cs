fd6f834259ec8a4c006fd0bc7ea725839ed2eac6
Chengkai Yang <yangchengkai@gmail.com>
2021-03-02 15:35:40 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace AzureRedisMessage
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Redis Message publisher.");
+
+         var redisClient = ConnectionMultiplexer.Connect("chyaredis.redis.cache.windows.net:6380,password=Q6UOzzdNSIiQa3sgw6znCeHINpfXKAStPkIEinjXths=,ssl=True,abortConnect=False");
+         var redisDb = redisClient.GetDatabase();
+
+         while (true)
+         {
+            Console.WriteLine("Type your message to publish (Type Q to quit):");
+
+            var msg = Console.ReadLine();
+
+            if (msg == "Q")
+            {
+               break;
+            }
+
+            redisDb.Publish("redisChannel", msg);
+         }
+
+      }
+   }
+}
